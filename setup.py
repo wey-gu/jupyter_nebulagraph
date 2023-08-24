96c01403ef3f07d4f0d118e5f5d7cd07da65e20b
Wey Gu <weyl.gu@gmail.com>
2023-08-24 13:09:05 +0800

    0.7.0: colab drawing and color scheme introduced

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ipython-ngql",
-    version="0.6",
+    version="0.7.0",
     author="Wey Gu",
     author_email="weyl.gu@gmail.com",
     description="Jupyter and iPython extension for NebulaGraph",
