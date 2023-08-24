fb0ee6392c20d3e5bb9c1b0d897f8b0335c7c424
Wey Gu <weyl.gu@gmail.com>
2023-08-24 12:43:00 +0800

    feat: colab support renderring fix and coloring nodes

--- ngql/magic.py
+++ ngql/magic.py
@@ -23,13 +23,14 @@ CONNECTION_POOL_CREATED = 1  # self.connection_pool newly created/recreated
 STYLE_PANDAS = "pandas"
 STYLE_RAW = "raw"
 
-COLORS = [
-    "#331832",
-    "#d81e5b",
-    "#f0544f",
-    "#C6D8D3",
-    "#FDF0D5",
-]
+COLORS = ["#E2DBBE", "#D5D6AA", "#9DBBAE", "#769FB6", "#188FA7"]
+
+
+def get_color(input_str):
+    hash_val = 0
+    for char in input_str:
+        hash_val = (hash_val * 31 + ord(char)) & 0xFFFFFFFF
+    return COLORS[hash_val % len(COLORS)]
 
 
 @magics_class
@@ -274,7 +275,10 @@ class IPythonNGQL(Magics, Configurable):
         try:
             import pandas as pd
             from pyvis.network import Network
-            from IPython.core.display import display, HTML
+            from IPython.display import display, IFrame, HTML
+
+            # import get_ipython
+            from IPython import get_ipython
 
         except ImportError:
             raise ImportError("Please install pyvis to draw the graph")
@@ -309,10 +313,20 @@ class IPythonNGQL(Magics, Configurable):
         # g.show_buttons(filter_='physics')
         # return g.show("nebulagraph_draw.html", notebook=True)
         g_html_string = g.generate_html("nebulagraph.html")
-        display(HTML(g_html_string))
-
         with open("nebulagraph.html", "w") as f:
             f.write(g_html_string)
+        # detect if we are in colab or not
+        try:
+            if "google.colab" in str(get_ipython()):
+                display(HTML(g_html_string))
+            else:
+                display(IFrame(src="nebulagraph.html", width="100%", height="500px"))
+        except Exception as e:
+            print(f"[WARN]: failed to display the graph\n { e }")
+            try:
+                display(IFrame(src="nebulagraph.html", width="100%", height="500px"))
+            except Exception as e:
+                print(f"[WARN]: failed to display the graph\n { e }")
 
         return g
 
@@ -324,15 +338,9 @@ class IPythonNGQL(Magics, Configurable):
             for tag in tags:
                 props.update(item.properties(tag))
 
-            def node_id_hash(input_str):
-                hash_val = 0
-                for char in input_str:
-                    hash_val = (hash_val * 31 + ord(char)) & 0xFFFFFFFF
-                return hash_val
-
-            color = COLORS[node_id_hash(node_id) % len(COLORS)]
-
-            g.add_node(node_id, label=node_id, title=str(props), color=color)
+            g.add_node(
+                node_id, label=node_id, title=str(props), color=get_color(node_id)
+            )
         elif isinstance(item, Relationship):
             src_id = item.start_vertex_id().cast()
             dst_id = item.end_vertex_id().cast()
@@ -340,9 +348,19 @@ class IPythonNGQL(Magics, Configurable):
             props = item.properties()
             # ensure start and end vertex exist in graph
             if not src_id in g.node_ids:
-                g.add_node(src_id)
+                g.add_node(
+                    src_id,
+                    label=str(src_id),
+                    title=str(src_id),
+                    color=get_color(src_id),
+                )
             if not dst_id in g.node_ids:
-                g.add_node(dst_id)
+                g.add_node(
+                    dst_id,
+                    label=str(dst_id),
+                    title=str(dst_id),
+                    color=get_color(dst_id),
+                )
             g.add_edge(src_id, dst_id, label=edge_name, title=str(props))
         elif isinstance(item, PathWrapper):
             for node in item.nodes():
